eac750ca34623770a1f07a428fce3c68d96f454c
siuwaikau <siuwaikau@gmail.com>
2022-04-28 01:43:46 +0100

    add test image and update source to point to the image

--- RPS-Template.py
+++ RPS-Template.py
@@ -10,7 +10,7 @@ model = load_model('keras_model.h5')
 # determined by the first position in the shape tuple, in this case 1.
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 # Replace this with the path to your image
-image = Image.open('<IMAGE_PATH>')
+image = Image.open('./test_scissors.jpg')
 #resize the image to a 224x224 with the same strategy as in TM2:
 #resizing the image to be at least 224x224 and then cropping from the center
 size = (224, 224)
